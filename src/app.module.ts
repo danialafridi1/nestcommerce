0e0dfcf00cab2a6fda62887b9029338036626632
Danial Afridi <iamdanialafridi@gmail.com>
2025-01-31 19:15:13 +0500

    feat: add TypeORM integration with PostgreSQL configuration in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRootAsync({
+    useFactory: () => ({
+      type: 'postgres',
+    entities:[],
+    host: 'localhost',
+    port: 8000,
+    username: 'postgres',
+    password: 'afridi4you',
+    database: 'nest_commerce',
+      synchronize: true,
+    })
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
